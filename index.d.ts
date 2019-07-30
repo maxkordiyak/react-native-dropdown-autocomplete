40a557378e386e43e99600e485b56406c88b438b
Paito Anderson <pj.paito@gmail.com>
2019-07-30 10:15:26 -0400

    Added Typescript Definitions
    Not sure if you are interested in supporting Typescript users...but this could really help some of us. Thanks!

---
+++ index.d.ts
@@ -0,0 +1,49 @@
+// Type definitions for react-native-dropdown-autocomplete 1.0
+
+import * as React from 'react';
+
+type AutocompleteProps = {
+    autoCorrect?: boolean,
+    highlightText?: boolean,
+    rightContent?: boolean,
+    resetOnSelect?: boolean,
+    minimumCharactersCount?: number,
+    waitInterval?: number,
+    placeholder?: string,
+    placeholderColor?: string,
+    spinnerSize?: string,
+    spinnerColor?: string,
+    listHeader?: string,
+    fetchDataUrl?: string,
+    fetchDataParams?: any,
+    noDataText?: string;
+    inputContainerStyle?: any;
+    inputStyle?: any;
+    spinnerStyle?: any;
+    noDataTextStyle?: any;
+    separatorStyle?: any;
+    listFooterStyle?: any;
+    listHeaderStyle?: any;
+    rightContentStyle?: any;
+    rightContentItemStyle?: any;
+    listHeaderTextStyle?: any;
+    overlayStyle?: any;
+    pickerStyle?: any;
+    containerStyle?: any;
+
+    scrollToInput: (ev: any) => void,
+    handleSelectItem: (item: any, index: number) => void,
+    onDropdownShow: () => void,
+    onDropdownClose: () => void,
+    renderIcon?: () => void,
+    valueExtractor?: (item: any) => void,
+    rightTextExtractor?: (item: any) => void,
+}
+
+export class Autocomplete extends React.Component<AutocompleteProps, any> {
+
+}
+
+export function withKeyboardAwareScrollView<P>(
+    component: React.ComponentType<P>
+): React.ComponentType<P>;
