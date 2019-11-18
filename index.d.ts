d6dd4c79cd3842bfa520fb144aaf48900124a82c
Paito Anderson <pj.paito@gmail.com>
2019-11-18 14:46:01 -0500

    Add disableFullscreenUI to typings

--- index.d.ts
+++ index.d.ts
@@ -4,23 +4,24 @@ import * as React from 'react';
 import { ViewStyle, TextStyle, StyleProp, KeyboardTypeOptions } from 'react-native';
 
 type AutocompleteProps = {
-    autoCorrect?: boolean,
+    autoCorrect?: boolean;
     keyboardType?: KeyboardTypeOptions;
-    highlightText?: boolean,
-    highLightColor?: string,
-    rightContent?: boolean,
-    resetOnSelect?: boolean,
-    minimumCharactersCount?: number,
-    waitInterval?: number,
-    placeholder?: string,
-    placeholderColor?: string,
-    spinnerSize?: string,
-    spinnerColor?: string,
-    listHeader?: string,
-    fetchDataUrl?: string,
+    highlightText?: boolean;
+    highLightColor?: string;
+    rightContent?: boolean;
+    resetOnSelect?: boolean;
+    minimumCharactersCount?: number;
+    waitInterval?: number;
+    placeholder?: string;
+    placeholderColor?: string;
+    spinnerSize?: string;
+    spinnerColor?: string;
+    listHeader?: string;
+    fetchDataUrl?: string;
     noDataText?: string;
     initialValue?: string;
     inputContainerStyle?: StyleProp<ViewStyle>;
+    disableFullscreenUI?: boolean;
     inputStyle?: StyleProp<TextStyle>;
     spinnerStyle?: StyleProp<ViewStyle>;
     noDataTextStyle?: StyleProp<TextStyle>;
@@ -35,15 +36,15 @@ type AutocompleteProps = {
     containerStyle?: StyleProp<ViewStyle>;
     scrollStyle?: StyleProp<ViewStyle>;
 
-    scrollToInput?: (ev: any) => void,
-    handleSelectItem: (item: any, index: number) => void,
-    onDropdownShow?: () => void,
-    onDropdownClose?: () => void,
-    onChangeText?: (search: string) => void,
-    renderIcon?: () => void,
-    valueExtractor?: (item: any) => void,
-    rightTextExtractor?: (item: any) => void,
-    fetchData?: (search: string) => Promise<any[]>,
+    scrollToInput?: (ev: any) => void;
+    handleSelectItem: (item: any, index: number) => void;
+    onDropdownShow?: () => void;
+    onDropdownClose?: () => void;
+    onChangeText?: (search: string) => void;
+    renderIcon?: () => void;
+    valueExtractor?: (item: any) => void;
+    rightTextExtractor?: (item: any) => void;
+    fetchData?: (search: string) => Promise<any[]>;
 }
 
 export class Autocomplete extends React.Component<AutocompleteProps, any> {
